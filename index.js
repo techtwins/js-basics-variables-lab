c05c39de31736c90c950cd2d38c82fcc916f470e
techtwins <gshevaughn@gmail.com>
2020-11-14 21:57:21 -0500

    done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
 const companyName = 'Scuber'
-var mostProfitableNeighborhood = 'Chelsea'
-var companyCeo = 'Susan Smith'
+let mostProfitableNeighborhood = 'Chelsea'
+let companyCeo = 'Susan Smith'
